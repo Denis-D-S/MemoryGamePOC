e33dfd9f3c49ab1a7546cb390ae94ec74f44fb7e
Denis-D-S <denisduartedasilva@hotmail.com>
2023-02-04 17:45:07 -0300

    cards flipando e funcionando

--- src/app/one-flipping-card/one-flipping-card.component.ts
+++ src/app/one-flipping-card/one-flipping-card.component.ts
@@ -6,14 +6,16 @@ import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
   styleUrls: ['./one-flipping-card.component.scss']
 })
 
-//inside this component, i need the logic to flip one card front and back, every 3 seconds...
 export class OneFlippingCardComponent implements OnInit {
 
   isFrontVisible = true; //variable for the first card
   isFrontVisible2 = true; // variable for the second card
 
-  constructor(private renderer: Renderer2, private el: ElementRef) {
+  constructor(private renderer: Renderer2, private el: ElementRef) {   //Renderer2 and ElementRef are Angular classes used for rendering and accessing DOM elements, respectively.
   }
+  // DICA: In this code, Renderer2 is used to add and remove CSS classes on a DOM element, which can be used to control
+  // the appearance of an element. ElementRef is used to access the DOM element represented by the component, so that
+  // the Renderer2 methods can be applied to it.
 
   ngOnInit() {
 
@@ -25,25 +27,39 @@ export class OneFlippingCardComponent implements OnInit {
     }, 3000);
   }
 
-  //ESTE MÉTODO ABAIXO FUNCIONA PERFEITAMENTE PARA RODAR O PRIMEIRO CARD...
-  //MAS NÃO FUNCIONA PARA RODAR O SEGUNDO CARD...
+  //DICA: a classe ".card-one" faz o card flipar. Porém é a classe ".card" que faz a ANIMAÇÃO do card virando bonitinho...
+  // Se você chamar apenas ".card-one" e não chamar ".card" junto, o card vai apenas ficar troccando sem animação nenhuma.
+  // Como se um card estivesse apenas se teleportando e assumindo o lugar do anterior. Horrível visualmente.
+
+  // OUTRA DICA:
+  // QUESTION: Please explain to me what this line does:  "const card = this.el.nativeElement.querySelector('.card-one .card')";
+  // ANSWER: This line of code retrieves the first DOM element that matches the selector .card-one .card from the component's
+  // associated HTML template. It uses the querySelector method, which is available on the nativeElement property of the
+  // ElementRef instance, this.el.
+  // The ElementRef is an Angular class that provides a way to interact with the underlying DOM element that is associated
+  // with a component. The nativeElement property is a reference to the actual DOM element, and can be used to access
+  // its properties and methods.
+  // In this case, the line of code is retrieving the first element that has both the classes card-one and card. The
+  // element is then stored in the card variable, which can be used later in the code to modify the element's styles or
+  // interact with it in other ways.
+
+
   flipFirstCard(){ //este método deveria ser para fazer rodar apenas o primeiro card...
-      this.isFrontVisible = !this.isFrontVisible;
-      const card = this.el.nativeElement.querySelector('.card-one .card');
-      if (this.isFrontVisible) {
+      this.isFrontVisible = !this.isFrontVisible; //true vira false...precisa fazer isso sempre que começa a rodar o método pois o boolean é como se fosse um gatilho que controla em que momento estamos: se o card está virado para cima ou para baixo...
+      const card = this.el.nativeElement.querySelector('.card-one .card'); //o segredo para fazer funcionar é combinar ".card-one" com ".card"...
+      if (this.isFrontVisible) { // se for true, roda código abaixo...
         this.renderer.removeClass(card, 'showGreen');
         this.renderer.addClass(card, 'showRed');
-      } else {
+      } else { // mas se for false, roda o código abaixo...
         this.renderer.removeClass(card, 'showRed');
         this.renderer.addClass(card, 'showGreen');
       }
   }
 
-  //E ESTE MÉTODO ABAIXO TAMBÉM FUNCIONA PERFEITAMENTE PARA RODAR O PRIMEIRO CARD...
-  //E TAMBÉM FUNCIONA PARA RODAR O SEGUNDO CARD...
+
   flipSecondCard(){ // este método deveria ser para faze rodar apenas o segundo card...
       this.isFrontVisible2 = !this.isFrontVisible2;
-      const card = this.el.nativeElement.querySelector('.card');
+      const card = this.el.nativeElement.querySelector('.card-two .card'); //o segredo para fazer funcionar é combinar ".card-two" com ".card"...
       if (this.isFrontVisible2) {
         this.renderer.removeClass(card, 'flip');
       } else {
